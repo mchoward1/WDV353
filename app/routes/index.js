4237f23db2d297113ad83f54ec043f7c38b4d5f9
Michael Howard <mchoward1@student.fullsail.edu>
2024-03-09 12:39:31 -0600

    1.4 Updates

---
+++ app/routes/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+//localhost:3000/api
+router.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'GET to API',
+        metadata: {hostname: req.hostname, method: req.method},
+    });
+});
+
+//localhost:3000/api/whateverfollows
+router.get('/:id', (req,res) => {
+    const {id} = req.params;
+    res.status(200).json({
+        message: 'Get by ID to /api',
+        id,
+        metadata: {hostname: req.hostname, method: req.method},
+    });
+});
+
+router.post('/', (req,res) => {
+    const {data} = req.body;
+    res.status(200).json({
+        message: 'POST to /api',
+        data,
+        metadata: {hostname: req.hostname, method: req.method},
+    });
+});
+
+router.put('/:id', (req,res) => {
+    const {id} = req.body;
+    res.status(200).json({
+        message: 'PUT by ID to /api',
+        id,
+        matadata: {hostname: req.hostname, method: req.method},
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const http = require('http');
+require('dotenv').config();
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    const { headers, url, method } = req;
-    console.log(headers, url, method);
-    res.end();
-});
+const server = http.createServer(app);
 
-const PORT = 3000;
-
-server.listen(PORT, () => {
-    console.log('listening on ' + PORT);
+server.listen(process.env.PORT, () => {
+    console.log('listening on ' + process.env.PORT);
 });
