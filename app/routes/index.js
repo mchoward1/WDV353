14a5f6529527b6590c5c7f7f1c87bf8977f8a12c
Michael Howard <mchoward1@student.fullsail.edu>
2024-03-10 16:48:04 -0500

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -21,7 +21,7 @@ router.get('/:id', (req,res) => {
 
 router.post('/', (req,res) => {
     const {data} = req.body;
-    res.status(200).json({
+    res.status(201).json({
         message: 'POST to /api',
         data,
         metadata: {hostname: req.hostname, method: req.method},
@@ -30,7 +30,7 @@ router.post('/', (req,res) => {
 
 router.put('/:id', (req,res) => {
     const {id} = req.body;
-    res.status(200).json({
+    res.status(201).json({
         message: 'PUT by ID to /api',
         id,
         matadata: {hostname: req.hostname, method: req.method},
