c157ef19824491b609d8aeb56bece46441d68dcd
Michael Howard <mchoward1@student.fullsail.edu>
2024-03-11 20:12:43 -0500

    Redid Assignment

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,47 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const {v4: uuidv4} = require('uuid');
+
+const birthdays = [];
+
+
 
 //localhost:3000/api
 router.get('/', (req, res) => {
     res.status(200).json({
         message: 'GET to API',
+        data: birthdays,
         metadata: {hostname: req.hostname, method: req.method},
     });
 });
 
 //localhost:3000/api/whateverfollows
 router.get('/:id', (req,res) => {
-    const {id} = req.params;
+    const {id} = req.params.id;
+    const birthday = birthdays.find((birthday => birthday.id == req.params.id));
     res.status(200).json({
         message: 'Get by ID to /api',
-        id,
+        id: birthday,
         metadata: {hostname: req.hostname, method: req.method},
     });
 });
 
+//Will need {"data": {"name": "Michael", "Birthday": "Feb, 11"}} in Postman body
 router.post('/', (req,res) => {
     const {data} = req.body;
+    birthdays.push({id: uuidv4(), ...data});
+    console.log('Birthdays: ', birthdays)
     res.status(201).json({
         message: 'POST to /api',
-        data,
+        data: birthdays[birthdays.length - 1],
         metadata: {hostname: req.hostname, method: req.method},
     });
 });
 
 router.put('/:id', (req,res) => {
-    const {id} = req.body;
+    const {id} = req.params.id;
+    const {data} = req.body;
+    const birthday = birthdays.find((birthday => birthday.id == req.params.id));
+    birthdays.splice(birthday);
+    birthdays.push({birthday, ...data});
     res.status(201).json({
         message: 'PUT by ID to /api',
-        id,
+        data: birthdays,
         matadata: {hostname: req.hostname, method: req.method},
     });
 });
 
 router.delete('/:id', (req,res) => {
-    const {id} = req.params;
-    res.status(202).json({
-        message: 'DELETE by ID from /api',
-        id,
+    const {id} = req.params.id;
+    const birthday = birthdays.findIndex((birthday => birthday.id == req.params.id));
+    birthdays.splice(birthday);
+    res.status(200).json({
+        message: 'Get by ID to /api',
+        id: birthdays,
         metadata: {hostname: req.hostname, method: req.method},
     });
 });
